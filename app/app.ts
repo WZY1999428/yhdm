5186b10fd956c2c6c5a6124c4e334ca494a5b698
wzy <2847656401@qq.com>
2022-07-04 00:10:29 +0800

    add

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import Vue from 'nativescript-vue'
-import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 declare let __DEV__: boolean;
 
@@ -7,5 +7,5 @@ declare let __DEV__: boolean;
 Vue.config.silent = !__DEV__
 
 new Vue({
-  render: (h) => h('frame', [h(Login)]),
+  render: (h) => h('frame', [h(Home)]),
 }).$start()
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,12 +1,11 @@
 <template>
-	<Page>
-		<ActionBar>
-			<Label text="Home" />
-		</ActionBar>
-
-		<GridLayout>
-			<Label :text="name"></Label>
-		</GridLayout>
+	<Page :actionBarHidden="true" :backgroundSpanUnderStatusBar="true">
+		<StackLayout backgroundColor="#eaeaea">
+			<FlexboxLayout class="ActionBar" alignItems="center" >
+				<Image class="icon" src="~/assets/home/search.png" />
+				<Label class="text">搜索</Label>
+			</FlexboxLayout>
+		</StackLayout>
 	</Page>
 </template>
 
@@ -16,24 +15,28 @@
 		Component
 	} from "vue-property-decorator";
 	@Component({
-		
+
 	})
 	export default class Home extends Vue {
-       name:string="123456789"
+		name: string = "123456789"
 	}
 </script>
 
 <style scoped lang="scss">
-	@import '@nativescript/theme/scss/variables/blue';
-
-	// Custom styles
-	.fas {
-		@include colorize($color: accent);
-	}
-
-	.info {
-		font-size: 20;
-		horizontal-align: center;
-		vertical-align: center;
+	.ActionBar {
+		width: 95%;
+		height: 100px;
+		padding: 0 20px;
+		background-color: #d1d1d1;
+		border-radius:80px;
+		padding: 0 20px;
+		.icon {
+			width: 40px;
+			height: 40px;
+			margin: 0 20px;
+		}
+		.text{
+			height: 90px;
+		}
 	}
 </style>
